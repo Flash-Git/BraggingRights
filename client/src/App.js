acefecb5099db4a0ed9f18c2d4ea571c2c2dc68b
James Quinn <flash.jaq@gmail.com>
2018-12-23 20:47:45 +0100

    Removed unused list systems

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
 import AppNavbar from './components/AppNavbar';
 import AppFooter from './components/AppFooter';
-import ShoppingList from './components/ShoppingList';
-import ItemModal from './components/ItemModal';
 
 import { Container } from "reactstrap"
 import { Provider } from "react-redux";
@@ -18,8 +16,7 @@ class App extends Component {
         <div className="App">
           <AppNavbar />
           <Container className="main">
-            <ItemModal />
-            <ShoppingList />
+            
           </Container>
           <footer className="footer">
             <AppFooter />
--- server.js
+++ server.js
@@ -2,8 +2,6 @@ const express = require("express");
 const mongoose = require("mongoose");
 const path = require("path");
 
-const items = require("./routes/api/items");
-
 const app = express();
 
 //Bodyparser middleware
@@ -19,19 +17,16 @@ mongoose
   .catch((err) => console.log(err));
 
 //Use routes
-app.use("/api/items", items);
 
 //Serve static assets if in prod
-
 if(process.env.NODE_ENV === "production") {
   //Set static folder
   app.use(express.static("client/build"));
 
   app.get("*", (req, res) => {
-	res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+	  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
-
 
 const port = process.env.PORT || 5000;
 
