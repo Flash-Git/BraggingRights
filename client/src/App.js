6597524fa49edb6f533df424b826bc2a3518154f
James Quinn <flash.jaq@gmail.com>
2018-12-24 23:10:52 +0100

    Setting landing page

--- client/src/App.js
+++ client/src/App.js
@@ -13,10 +13,10 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <Provider store={store}>
-        <div className="App">
-          <AppNavbar />
-          <Container className="main">
+      <Provider store={store} style={{margin:0, padding:0}}>
+        <div className="App" style={{margin:0, padding:0}}>
+          <AppNavbar style={{margin:0, padding:0}} />
+          <Container className="main" style={{margin:0, padding:0}}>
             <Landing />
           </Container>
           <footer>
